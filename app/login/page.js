e88ca99a7ff5b7bfccfdffab8a15574b93110867
allwinjandrews <allwinandrews@gmail.com>
2023-09-30 00:28:04 +0100

    Initial Commit

---
+++ app/login/page.js
@@ -0,0 +1,32 @@
+"use client"; // This is a client component 👈🏽
+import React, { useState } from "react";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    // Implement login logic here
+  };
+
+  return (
+    <div>
+      <h1>Login to Your Account</h1>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
+
+export default Login;
